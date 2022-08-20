517c843f1ba0dc1a6cfce51e6dd3cecbf1f6d5af
Nicolás Emiliano Gudiño <nicolas_gudino@hotmail.com>
2022-08-20 14:01:03 -0300

    Routing

--- ProyectoIntegrador/FrontEnd/src/app/app.module.ts
+++ ProyectoIntegrador/FrontEnd/src/app/app.module.ts
@@ -13,7 +13,9 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { SkillsComponent } from './components/skills/skills.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HttpClient, HttpClientModule} from '@angular/common/http'
+import { HttpClient, HttpClientModule} from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component'
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { HttpClient, HttpClientModule} from '@angular/common/http'
     EducacionComponent,
     SkillsComponent,
     ProyectosComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
