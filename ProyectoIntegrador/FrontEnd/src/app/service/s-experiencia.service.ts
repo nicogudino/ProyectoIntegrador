077c30ffc51e08b36532e3fa1ae1b7c435ed53d2
Nicolás Emiliano Gudiño <nicolas_gudino@hotmail.com>
2022-09-26 23:39:11 -0300

    Experiencia 2 agregar una nueva

--- ProyectoIntegrador/FrontEnd/src/app/service/s-experiencia.service.ts
+++ ProyectoIntegrador/FrontEnd/src/app/service/s-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class SExperienciaService {
-  expURL = 'http://localhost:8080/explab'
+  expURL = 'http://localhost:8080/explab/'
 
   constructor(private httpClient: HttpClient) { }
 
@@ -22,11 +22,11 @@ export class SExperienciaService {
     return this.httpClient.post<any>(this.expURL + 'create', experiencia);
   }
 
-  public update(id: number, experiencia: Experiencia):Observable<any>{
-    return this.httpClient.put<any>(this.expURL + `update/"${id}`, experiencia);
+  public update(id: number, experiencia: Experiencia): Observable<any>{
+    return this.httpClient.put<any>(this.expURL + `update/${id}`, experiencia);
   }  
 
-  public delete(id:number): Observable<any>{
+  public delete(id: number): Observable<any>{
     return this.httpClient.delete<any>(this.expURL + `delete/${id}`);
   }
 }
